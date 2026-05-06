05fd390ade612cdc80a8dbcf805f6540d193f296
Paul Orozco <orozco.isboseth@gmail.com>
2026-05-06 11:21:57 -0700

    Create external_sync_service.test.js

---
+++ tests/external_sync_service.test.js
@@ -0,0 +1,11 @@
+const service = require('../src/services/external_sync_service');
+
+const result = service.sendTransaction({
+  id: 123,
+  type: 'salesorder'
+});
+
+console.assert(result.success === true, 'Service should return success');
+console.assert(result.externalId, 'Service should return an externalId');
+
+console.log('External sync service test passed:', result);
